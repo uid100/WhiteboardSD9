024851b4f6e48b47bc90196eb9d682fd1a50f8e0
Dan Sullivan <daniel.f.sullivan@gmail.com>
2021-02-11 09:39:41 -0800

    Updated primes tests and contains bug fix

--- Primes/PrimeNumbers.cs
+++ Primes/PrimeNumbers.cs
@@ -6,9 +6,36 @@ namespace Primes
     {
         public static bool IsPrime(int number)
         {
-            for (int i = 2; i < number-1; i++)
-                if (number % i == 0) return false;
+            //int maxRange = (int)Math.Sqrt(number);
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
             return true;
         }
+
+        public static int SumOfPrimes(int maxNumber)
+        {
+            int sum = 0;
+
+            for (int i = 2; i <= maxNumber; i++)
+            {
+                //sum += IsPrime(i) ? i : 0;
+               
+                if( IsPrime(i) )
+                {
+                    sum += i;
+                }
+                
+            }
+
+            return sum;
+        }
     }
+
 }
--- PrimesTest/UnitTest1.cs
+++ PrimesTest/UnitTest1.cs
@@ -6,9 +6,61 @@ namespace PrimesTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod17()
+        public void TestIsPrime_17()
         {
             Assert.AreEqual(true, PrimeNumbers.IsPrime(17));
         }
+        [TestMethod]
+        public void TestIsPrime_4()
+        {
+            Assert.AreEqual(false, PrimeNumbers.IsPrime(4));
+        }
+        [TestMethod]
+        public void TestIsPrime_8()
+        {
+            Assert.AreEqual(false, PrimeNumbers.IsPrime(8));
+        }
+        [TestMethod]
+        public void TestIsPrime_9()
+        {
+            Assert.AreEqual(false, PrimeNumbers.IsPrime(9));
+        }
+
+        [TestMethod]
+        public void TestSumOfPrimes_8()
+        {            
+            Assert.AreEqual(17, PrimeNumbers.SumOfPrimes(8));
+        }
+        [TestMethod]
+        public void TestSumOfPrimes_6()
+        {
+            Assert.AreEqual(10, PrimeNumbers.SumOfPrimes(6));
+        }
+        [TestMethod]
+        public void TestSumOfPrimes_2()
+        {
+            Assert.AreEqual(2, PrimeNumbers.SumOfPrimes(2));
+        }
+        [TestMethod]
+        public void TestSumOfPrimes_0()
+        {
+            Assert.AreEqual(0, PrimeNumbers.SumOfPrimes(0));
+        }
+        [TestMethod]
+        public void TestSumOfPrimes_1()
+        {
+            Assert.AreEqual(0, PrimeNumbers.SumOfPrimes(1));
+        }
+        [TestMethod]
+        public void TestSumOfPrimes_neg1()
+        {
+            Assert.AreEqual(0, PrimeNumbers.SumOfPrimes(-1));
+        }
+        [TestMethod]
+        public void TestSumOfPrimes_4()
+        {
+            Assert.AreEqual(5, PrimeNumbers.SumOfPrimes(4));
+        }
+
     }
 }
